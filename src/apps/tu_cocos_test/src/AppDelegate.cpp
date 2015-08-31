3471729a1873f3b0720d893cee79cdb0b434ab68
maodazheng <maodazheng@163.com>
2015-08-31 17:25:04 +0800

    添加test代码

---
+++ src/apps/tu_cocos_test/src/AppDelegate.cpp
@@ -0,0 +1,131 @@
+#include "precompiled.h"
+#include "AppDelegate.h"
+
+#include <vector>
+#include <string>
+
+#include "HelloWorldScene.h"
+#include "AppMacros.h"
+
+USING_NS_CC;
+using namespace std;
+
+#if CC_TARGET_PLATFORM==CC_PLATFORM_WIN32
+static inline std::string ConvertPathFormatToUnixStyle(const std::string& path)
+{
+	std::string ret = path;
+	int len = ret.length();
+	for (int i = 0; i < len; ++i)
+	{
+		if (ret[i] == '\\')
+		{
+			ret[i] = '/';
+		}
+	}
+	return ret;
+}
+
+static std::string GetCurrentDirectory()
+{
+	char current_directory[MAX_PATH];
+	::GetCurrentDirectoryA(MAX_PATH, current_directory);
+	return ConvertPathFormatToUnixStyle(std::string(current_directory)+"/");
+}
+#endif // _DEBUG
+
+AppDelegate::AppDelegate() {
+
+}
+
+AppDelegate::~AppDelegate() 
+{
+}
+
+void AppDelegate::initGLContextAttrs()
+{
+    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
+
+    GLView::setGLContextAttrs(glContextAttrs);
+}
+
+bool AppDelegate::applicationDidFinishLaunching() {
+    // initialize director
+    auto director = Director::getInstance();
+    auto glview = director->getOpenGLView();
+    if(!glview) {
+        glview = GLViewImpl::create("Cpp Empty Test");
+        director->setOpenGLView(glview);
+    }
+
+    director->setOpenGLView(glview);
+
+    // Set the design resolution
+    glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::NO_BORDER);
+
+	Size frameSize = glview->getFrameSize();
+    
+    vector<string> searchPath;
+
+    // In this demo, we select resource according to the frame's height.
+    // If the resource size is different from design resolution size, you need to set contentScaleFactor.
+    // We use the ratio of resource's height to the height of design resolution,
+    // this can make sure that the resource's height could fit for the height of design resolution.
+
+    // if the frame's height is larger than the height of medium resource size, select large resource.
+	if (frameSize.height > mediumResource.size.height)
+	{
+        searchPath.push_back(largeResource.directory);
+
+        director->setContentScaleFactor(MIN(largeResource.size.height/designResolutionSize.height, largeResource.size.width/designResolutionSize.width));
+	}
+    // if the frame's height is larger than the height of small resource size, select medium resource.
+    else if (frameSize.height > smallResource.size.height)
+    {
+        searchPath.push_back(mediumResource.directory);
+        
+        director->setContentScaleFactor(MIN(mediumResource.size.height/designResolutionSize.height, mediumResource.size.width/designResolutionSize.width));
+    }
+    // if the frame's height is smaller than the height of medium resource size, select small resource.
+	else
+    {
+        searchPath.push_back(smallResource.directory);
+
+        director->setContentScaleFactor(MIN(smallResource.size.height/designResolutionSize.height, smallResource.size.width/designResolutionSize.width));
+    }
+    
+#if CC_TARGET_PLATFORM==CC_PLATFORM_WIN32
+	FileUtils::getInstance()->setDefaultResourceRootPath(GetCurrentDirectory());
+#endif
+    // set searching path
+    FileUtils::getInstance()->setSearchPaths(searchPath);
+	
+    // turn on display FPS
+    director->setDisplayStats(true);
+
+    // set FPS. the default value is 1.0/60 if you don't call this
+    director->setAnimationInterval(1.0 / 60);
+
+    // create a scene. it's an autorelease object
+    auto scene = HelloWorld::scene();
+
+    // run
+    director->runWithScene(scene);
+
+    return true;
+}
+
+// This function will be called when the app is inactive. When comes a phone call,it's be invoked too
+void AppDelegate::applicationDidEnterBackground() {
+    Director::getInstance()->stopAnimation();
+
+    // if you use SimpleAudioEngine, it must be pause
+    // SimpleAudioEngine::sharedEngine()->pauseBackgroundMusic();
+}
+
+// this function will be called when the app is active again
+void AppDelegate::applicationWillEnterForeground() {
+    Director::getInstance()->startAnimation();
+
+    // if you use SimpleAudioEngine, it must resume here
+    // SimpleAudioEngine::sharedEngine()->resumeBackgroundMusic();
+}
